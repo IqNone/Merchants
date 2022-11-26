c11c071dc066aa7a1f79f695c2a5f26f5ecc33fe
Adrian Iriciuc <adrian.iriciuc@aurea.com>
2022-11-26 05:06:08 +0200

    add animals

---
+++ Source/Merchants/Private/Creatures/CreatureSpawner.cpp
@@ -0,0 +1,81 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Creatures/CreatureSpawner.h"
+#include "Creatures/Creature.h"
+#include "Components/SphereComponent.h"
+#include "Engine/World.h"
+#include "Components/BillboardComponent.h"
+#include "EnvironmentQuery/EnvQuery.h"
+#include "EnvironmentQuery/EnvQueryManager.h"
+
+// Sets default values
+ACreatureSpawner::ACreatureSpawner()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
+
+	SpawnArea = CreateDefaultSubobject<USphereComponent>(TEXT("SpehereComp"));
+	SpawnArea->SetSphereRadius(1000.f);
+	RootComponent = SpawnArea;
+
+	Billboard = CreateDefaultSubobject<UBillboardComponent>(TEXT("Billboard"));
+	Billboard->SetupAttachment(GetRootComponent());
+	
+	PreviewMesh = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("PreviewMeshComp"));
+	PreviewMesh->SetupAttachment(GetRootComponent());
+
+	MaxRoamingRadius = 50000.f;
+	RespawnDeltaSeconds = 10;
+
+	SpawnQueryRadiusParamName = "SpawnRadius";
+}
+
+// Called when the game starts or when spawned
+void ACreatureSpawner::BeginPlay()
+{
+	Super::BeginPlay();	
+
+	if (SpawnQuery) 
+	{
+		SpawnQueryRequest = FEnvQueryRequest(SpawnQuery);
+
+		Spawn();
+	}	
+}
+
+// Called every frame
+void ACreatureSpawner::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void ACreatureSpawner::Spawn()
+{
+	if (!HasAuthority())
+	{
+		return;
+	}
+
+	SpawnQueryRequest.SetFloatParam(SpawnQueryRadiusParamName, SpawnArea->GetScaledSphereRadius());
+	SpawnQueryRequest.Execute(EEnvQueryRunMode::RandomBest25Pct, this, &ACreatureSpawner::SpawnQueryFinished);
+}
+
+void ACreatureSpawner::SpawnQueryFinished(TSharedPtr<FEnvQueryResult> SpawnQueryResult)
+{
+	if (!SpawnQueryResult->IsSuccsessful())
+	{
+		return;
+	}
+	FVector SpawnLocation = SpawnQueryResult->GetItemAsLocation(0);
+
+	FActorSpawnParameters SpawnParams;
+	SpawnParams.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AdjustIfPossibleButAlwaysSpawn;
+
+	ACreature* SpawnedCreature = GetWorld()->SpawnActor<ACreature>(CreatureClass, SpawnLocation, FRotator::ZeroRotator, SpawnParams);
+	//if (SpawnedCreature)
+	//{
+	//	SpawnedCreature->GetHealthComp()->OnActorKilled.AddDynamic(this, &AELSpawner::HandleCreatureKilled);
+	//}
+}
---
+++ Source/Merchants/Public/Creatures/CreatureSpawner.h
@@ -0,0 +1,62 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "EnvironmentQuery/EnvQueryManager.h"
+#include "CreatureSpawner.generated.h"
+
+class ACreature;
+class USphereComponent;
+class UEnvQuery;
+
+UCLASS()
+class MERCHANTS_API ACreatureSpawner : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ACreatureSpawner();
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Spawn")
+	TSubclassOf<ACreature> CreatureClass;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Spawn")
+	USkeletalMeshComponent* PreviewMesh;
+
+	UPROPERTY(VisibleDefaultsOnly, Category = "Spawn")
+	USphereComponent* SpawnArea;
+
+	UPROPERTY(EditAnywhere, Category = "Spawn")
+	float RespawnDeltaSeconds;
+
+	UPROPERTY(EditAnywhere, Category = "Spawn")
+	float MaxRoamingRadius;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Spawn")
+	UEnvQuery* SpawnQuery;
+
+	UPROPERTY(VisibleDefaultsOnly, Category = "Spawn")
+	FName SpawnQueryRadiusParamName;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+private:
+
+	UBillboardComponent* Billboard;
+
+	FEnvQueryRequest SpawnQueryRequest;
+
+	void Spawn();
+
+	void SpawnQueryFinished(TSharedPtr<FEnvQueryResult> SpawnQueryResult);
+
+};
